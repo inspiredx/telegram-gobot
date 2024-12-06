b7fd016801083ca92b90c894bd19c58613311297
root <georgiykapustinx@gmail.com>
2024-12-06 14:27:02 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module my_telegram_bot
+
+go 1.18
+
+require gopkg.in/telebot.v4 v4.0.0-beta.4
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"time"
+
+	tele "gopkg.in/telebot.v4"
+)
+
+func main() {
+	token := "7824776293:AAHIfprFjFTWYBFA05KaHs6cRPN-_xOoe1Q"
+
+	// Настройки бота
+	pref := tele.Settings{
+		Token:  token,
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	// Создание бота
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatalf("Ошибка при создании бота: %s", err)
+	}
+
+	// Логирование всех входящих обновлений
+	b.Use(func(next tele.HandlerFunc) tele.HandlerFunc {
+		return func(c tele.Context) error {
+			log.Printf("Получено сообщение от %s: %s", c.Sender().Username, c.Text())
+			return next(c)
+		}
+	})
+
+	// Массивы для гарниров и основных блюд
+	sideDishes := []string{"Картошка", "Рис", "Овощи", "Гречка", "Макароны"}
+	mainDishes := []string{"Стейк", "Курица", "Рыба", "Говядина", "Паста"}
+
+	// Обработка команды /start
+	b.Handle("/start", func(c tele.Context) error {
+		// Получаем имя пользователя или юзернейм
+		username := c.Sender().Username
+		if username == "" {
+			// Если юзернейм пустой, используем имя пользователя
+			username = c.Sender().FirstName
+		}
+		return c.Send("Привет, " + username + "!")
+	})
+
+	// Обработка команды /food
+	b.Handle("/food", func(c tele.Context) error {
+		// Генерация случайных индексов для выбора гарнира и основного блюда
+		rand.Seed(time.Now().UnixNano())
+		sideDish := sideDishes[rand.Intn(len(sideDishes))]
+		mainDish := mainDishes[rand.Intn(len(mainDishes))]
+
+		// Отправка результата пользователю
+		return c.Send("Гарнир: " + sideDish + ", Основное блюдо: " + mainDish)
+	})
+
+	// Запуск бота
+	log.Println("Бот запущен...")
+	b.Start()
+}
